f1ef0f6b9271169aae439606324c03bf3d577ae0
Erick <ahmederick00@gmail.com>
2022-03-19 00:22:33 +0100

    minor folder rearrangements

--- code/framework.h
+++ code/framework.h
@@ -6,13 +6,10 @@
 
 #include "targetver.h"
 #define WIN32_LEAN_AND_MEAN             // Escludere gli elementi usati raramente dalle intestazioni di Windows
-
+// File di intestazione di Windows
+#include <windows.h>
+// File di intestazione Runtime C
+#include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
-
-#include <windows.h>
-#include <stdint.h>
-#include <stdlib.h>
-//#include <crtdbg.h> // used for detecting memory leaks in VS, check MSDN; use: _CrtDumpMemoryLeaks()
-
