f12a54bd67a532b5d263a258677a5d578cce517f
Erick <ahmederick00@gmail.com>
2022-03-21 14:16:42 +0100

    add windows.h and debugapi.h libraries

--- code/framework.h
+++ code/framework.h
@@ -1,13 +1,23 @@
+/* =================================================================================
+    This file is a part of the Handmade Hero project series made by Casey Muratori
+    that I am building using his videos as a reference and adding a personal touch!
+
+    - INFORMATIONS -
+    File: framework.h
+    Author: Erick Ahmed
+    Creation: 08/03/2022
+    Last revision: 21/03/2022
+=================================================================================== */
+
+
 #pragma once
 
-#include "targetver.h"
-#define WIN32_LEAN_AND_MEAN
-
 #include <windows.h>
+#include <debugapi.h>
 
 #include <stdint.h>
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
-#include <crtdbg.h>
+//#include <crtdbg.h> check why it wont compile
